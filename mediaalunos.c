ea950264179260eafa2fcab60b8b1e29a0765320
programadorsamuca <samuelrochapimentel06@gmail.com>
2025-03-12 13:37:29 -0300

    Update mediaalunos.c

--- mediaalunos.c
+++ mediaalunos.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+int main(){
+    int Idade;
+    float Altura;
+    char Nome[50];
+    int Matricula;
 
+    printf("Digite sua Idade: \n");
+    scanf("%d", &Idade);
+
+    printf("Digite sua Altura: \n");
+    scanf("%f", &Altura);
+
+    printf("Digite seu Nome: \n");
+    scanf("%s", &Nome);
+
+    printf("Digite sua Matrícula: \n");
+    scanf("%d", &Matricula);
+
+    printf("Nome do Aluno:  %s - Idade do Aluno: %d \n", Nome, Idade);
+    printf("Matrícula: %d - Altura: %f", Matricula, Altura);
+
+    return 0;
+
+
+}
